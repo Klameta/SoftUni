d1c6c274e9ad4eeee5eaf097ca9c78e9fcb36755
Klameta <74143760+Klameta@users.noreply.github.com>
2022-06-09 01:36:03 +0300

    Add files via upload

---
+++ modul_1/10/02 Half Sum Element/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02_Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nInput = int.Parse(Console.ReadLine());
+            int maxNum = int.MinValue;
+            int sum = 0;
+            for (int i = 0; i < nInput; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+                if (num>maxNum)
+                {
+                    maxNum = num;
+                }
+                
+            }
+            int diff = Math.Abs(sum - maxNum);
+            if (diff==maxNum)
+            {
+                Console.WriteLine($"Yes");
+                Console.WriteLine($"Sum = {maxNum}");
+            }
+            else
+            {
+                Console.WriteLine($"No");
+                Console.WriteLine($"Diff = {Math.Abs(maxNum-diff)}");
+            }
+        }
+    }
+}
---
+++ modul_1/10/04 Clever Lily/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04_Clever_Lily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int old = int.Parse(Console.ReadLine());
+            double price = double.Parse(Console.ReadLine());
+            int pForOne = int.Parse(Console.ReadLine());
+
+            int evenBd = 0;
+            int oddBd = 0;
+            int moneySum = 0;
+            int brother = 0;
+
+            for (int i = 1; i <= old; i++)
+            {
+                if (i%2==0)
+                {
+                    evenBd+=10;
+                    moneySum += evenBd;
+                    brother ++;
+                }
+                else
+                {
+                    oddBd++;
+                }
+            }
+            int finalSum = (moneySum + oddBd * pForOne) - brother;
+            if (price<=finalSum)
+            {
+                Console.WriteLine($"Yes! {finalSum-price:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {price-finalSum:F2}");
+            }
+        }
+    }
+}
