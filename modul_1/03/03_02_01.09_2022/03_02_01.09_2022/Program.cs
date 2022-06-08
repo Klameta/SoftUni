ff5818f0559405d27c4fd821ae9004bf2d8c6886
Klameta <74143760+Klameta@users.noreply.github.com>
2022-06-09 01:25:31 +0300

    Add files via upload

---
+++ modul_1/03/03_02_01.09_2022/03_02_01.09_2022/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03_02_01._09_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double g = r * 180 / Math.PI;
+            Console.WriteLine(g);
+        }
+    }
+}
