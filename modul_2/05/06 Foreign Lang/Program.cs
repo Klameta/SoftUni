c523b2a3c72a26eb04d6f3aeaba69b5a4d23dfc7
Klameta <74143760+Klameta@users.noreply.github.com>
2022-06-09 01:51:39 +0300

    Add files via upload

---
+++ modul_2/05/06 Foreign Lang/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Foreign_Lang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var country = Console.ReadLine();
+            switch (country)
+            {
+                case "England":
+                case "USA":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                case "Mexico":
+                case "Argentina":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
