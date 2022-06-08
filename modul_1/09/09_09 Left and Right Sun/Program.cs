d80a7f014fc8789294bbeb1541f1a38d5a3da906
Klameta <74143760+Klameta@users.noreply.github.com>
2022-06-09 01:34:13 +0300

    Add files via upload

---
+++ modul_1/09/09_09 Left and Right Sun/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _09_09_Left_and_Right_Sun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nInput = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+
+
+            for (int i = 0; i < nInput; i++)
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                leftSum += n1;
+            }
+
+            for (int i = 0; i < nInput; i++)
+            {
+                int n2 = int.Parse(Console.ReadLine());
+                rightSum += n2;
+            }
+
+            if (leftSum==rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(leftSum-rightSum)}");
+            }
+        }
+    }
+}
