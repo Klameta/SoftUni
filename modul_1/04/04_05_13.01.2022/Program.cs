e2b18ba05249d4b4e87f6e16894a850a6e09b2d2
Klameta <74143760+Klameta@users.noreply.github.com>
2022-06-09 01:26:53 +0300

    Add files via upload

---
+++ modul_1/04/04_05_13.01.2022/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04_05_13._01._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nInput = int.Parse(Console.ReadLine());
+            if (nInput<100)
+            {
+                Console.WriteLine($"Less than 100");
+            }
+            else if (nInput <=200)
+            {
+                Console.WriteLine($"Between 100 and 200");
+            }
+            else
+            {
+                Console.WriteLine($"Greater than 200");
+            }
+        }
+    }
+}
---
+++ modul_1/04/04_06_13.01.2022/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04_06_13._01._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nInput = double.Parse(Console.ReadLine());
+            if (nInput <= 10)
+            {
+                Console.WriteLine($"slow");
+            }
+            else if (nInput<=50)
+            {
+                Console.WriteLine($"average");
+            }
+            else if (nInput<=150)
+            {
+                Console.WriteLine($"fast");
+
+            }
+            else if (nInput<=1000)
+            {
+                Console.WriteLine($"ultra fast");
+            }
+            else
+            {
+                Console.WriteLine($"extremely fast");
+            }
+        }
+    }
+}
