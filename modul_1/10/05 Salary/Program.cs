588aae6e968bb872ecdaf837a92e25b6abe0449a
Klameta <74143760+Klameta@users.noreply.github.com>
2022-06-09 01:37:28 +0300

    Add files via upload

---
+++ modul_1/10/05 Salary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _05_Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nTabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            int fb = 0;
+            int inst = 0;
+            int redd = 0;
+
+            for (int i = 0; i < nTabs; i++)
+                
+            {
+                string tab = Console.ReadLine();
+                switch (tab)
+                {
+                    case "Facebook":
+                        salary -= 150;
+                        break;
+                    case "Instagram":
+                        salary -= 100;
+                        break;
+                    case "Reddit":
+                        salary -= 50;
+                        break;
+                }
+                if (salary<=0)
+                {
+                    Console.WriteLine($"You have lost your salary.");
+                    break;
+
+                }
+            }
+
+            if (salary>0)
+            {
+                Console.WriteLine($"{salary}");
+
+            }
+        }
+    }
+}
---
+++ modul_1/10/06 Oscars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06_Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
