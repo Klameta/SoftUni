f1ce04c9d6b71417eb374a868cf7016877ca5aec
Klameta <74143760+Klameta@users.noreply.github.com>
2022-07-17 02:55:41 +0300

    its 3 am almos. gonna take a break

---
+++ m2/22. Objects and Classes - More Exercise/03. Speed Racing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Speed_Racing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
