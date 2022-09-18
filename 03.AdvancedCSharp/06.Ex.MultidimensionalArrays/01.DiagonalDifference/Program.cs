256e4cabf863d66ebf7d192e88ca93d30a8b74c6
Klameta <ivanavalcheva@gmail.com>
2022-09-18 23:20:44 +0300

    01 started

---
+++ 03.AdvancedCSharp/06.Ex.MultidimensionalArrays/01.DiagonalDifference/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsCols = int.Parse(Console.ReadLine());
+            int[,] squareMatrix = new int[rowsCols, rowsCols];
+
+        }
+    }
+}
