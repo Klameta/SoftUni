5db6d00ca7053bfca7d7168fcdb16da6ae458f67
Klameta <ivanavalcheva@gmail.com>
2022-09-18 19:40:07 +0300

    08. still working...

--- 03.AdvancedCSharp/04.Ex.StacksAndQueues/08.BalancedParenthesis/Program.cs
+++ 03.AdvancedCSharp/04.Ex.StacksAndQueues/08.BalancedParenthesis/Program.cs
@@ -10,6 +10,63 @@ namespace _08.BalancedParenthesis
     {
         static void Main(string[] args)
         {
+
+            char[] input = Console.ReadLine().ToCharArray();
+            Stack<char> leftBrackets = new Stack<char>();
+            int currIndex = 0;
+            bool endOfArr = false;
+
+
+            while (input[currIndex]!= ')' && input[currIndex] != ']' && input[currIndex] != '}' && !endOfArr)
+            {
+                CheckIfIndexIsBeyondArrLenght(input, ref currIndex, ref endOfArr);
+                char leftBracket = input[currIndex-1];
+                leftBrackets.Push(leftBracket);
+
+            }
+
+            bool balanced = true;
+            CheckIfIndexIsBeyondArrLenght(input, ref currIndex, ref endOfArr);
+
+            while (balanced && !endOfArr && leftBrackets.Count>0)
+            {
+                char currBracket = leftBrackets.Pop();
+
+                if (currBracket== '{' && input[currIndex-1]!='}') 
+                {
+                    balanced = false;
+                }
+                else if (currBracket=='[' && input[currIndex-1]!=']')
+                {
+                    balanced = false;
+                }
+                else if (currBracket=='(' && input[currIndex-1]!=')')
+                {
+                    balanced = false;
+                }
+                CheckIfIndexIsBeyondArrLenght(input,ref currIndex,ref endOfArr);
+            }
+
+            if (balanced && leftBrackets.Count==0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+        private static void CheckIfIndexIsBeyondArrLenght(char[] input, ref int currIndex, ref bool endOfArr)
+        {
+            if (currIndex == input.Count() - 1)
+            {
+                endOfArr = true;
+            }
+            else
+            {
+                currIndex++;
+            }
         }
     }
 }
