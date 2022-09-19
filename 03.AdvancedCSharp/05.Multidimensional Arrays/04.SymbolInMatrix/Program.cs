3ce854b241ab9cb34dfef8f82f2b360c5b6e6781
Klameta <ivanavalcheva@gmail.com>
2022-09-20 01:29:23 +0300

    04 almost done 05 done

---
+++ 03.AdvancedCSharp/05.Multidimensional Arrays/04.SymbolInMatrix/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace o4.SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowsCols = int.Parse(Console.ReadLine());
+            char[,] squareCharMatrix = new char[rowsCols, rowsCols];
+
+            for (int i = 0; i < rowsCols; i++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+                for (int k = 0; k < rowsCols; k++)
+                {
+                    squareCharMatrix[i, k] = input[k];
+                }
+            }
+
+            char searchSymbol = char.Parse(Console.ReadLine());
+            string cords = string.Empty;
+            for (int i = 0; i < rowsCols; i++)
+            {
+                for (int k = 0; k < rowsCols; k++)
+                {
+                    if (squareCharMatrix[i, k] == searchSymbol)
+                    {
+                        cords = $"({i}, {k})";
+                    }
+                }
+            }
+
+            if (cords!=string.Empty)
+            {
+                Console.WriteLine(cords);
+            }
+            else
+            {
+                Console.WriteLine($"{searchSymbol} does not occur in the matrix");
+            }
+        }
+    }
+}
