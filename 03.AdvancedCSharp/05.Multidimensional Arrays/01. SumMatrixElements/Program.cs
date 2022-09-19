05bf5251cce575c5b7378bd3f3d3ccfb1d107246
Klameta <ivanavalcheva@gmail.com>
2022-09-20 00:15:12 +0300

    01,02,03 done

---
+++ 03.AdvancedCSharp/05.Multidimensional Arrays/01. SumMatrixElements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _01._SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols= input[1];
+            int[,] numMatric = new int[rows, cols];
+            int sum=0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] inputMatric = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int k = 0; k < cols; k++)
+                {
+                    numMatric[i, k] = inputMatric[k];
+                    sum += inputMatric[k];
+                }
+            }
+
+            Console.WriteLine($"{rows}\n{cols}\n{sum}");
+        }
+    }
+}
