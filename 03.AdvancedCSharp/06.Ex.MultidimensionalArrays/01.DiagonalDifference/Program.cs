711557c18bea096a0c094839d2040b6d931ad560
Klameta <ivanavalcheva@gmail.com>
2022-09-20 20:05:31 +0300

    01,02 done

--- 03.AdvancedCSharp/06.Ex.MultidimensionalArrays/01.DiagonalDifference/Program.cs
+++ 03.AdvancedCSharp/06.Ex.MultidimensionalArrays/01.DiagonalDifference/Program.cs
@@ -13,6 +13,27 @@ namespace _01.DiagonalDifference
             int rowsCols = int.Parse(Console.ReadLine());
             int[,] squareMatrix = new int[rowsCols, rowsCols];
 
+            for (int row = 0; row < rowsCols; row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(n => int.Parse(n))
+                    .ToArray();
+                for (int col = 0; col < rowsCols; col++)
+                {
+                    squareMatrix[row, col] = input[col];
+                }
+            }
+            int leftRightSum = 0;
+            int rightLeftSum = 0;
+
+            for (int leftRight = 0, rightLeft = rowsCols - 1; leftRight < rowsCols; leftRight++, rightLeft--)
+            {
+                leftRightSum += squareMatrix[leftRight, leftRight];
+                rightLeftSum += squareMatrix[leftRight, rightLeft];
+            }
+
+            Console.WriteLine($"{Math.Abs(leftRightSum - rightLeftSum)}");
         }
     }
 }
