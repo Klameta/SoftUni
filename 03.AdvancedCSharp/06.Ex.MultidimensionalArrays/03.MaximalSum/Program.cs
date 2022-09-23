3d92372a06cb11b3286f19b099634a1c1ffaf7ea
Klameta <ivanavalcheva@gmail.com>
2022-09-23 17:53:58 +0300

    04,05 done

---
+++ 03.AdvancedCSharp/06.Ex.MultidimensionalArrays/03.MaximalSum/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace _03.MaximalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = dimentions[0];
+            int cols = dimentions[1];
+            int[,] numMatrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    numMatrix[row, col] = input[col];
+                }
+            }
+
+            int innerCols = 0;
+            int innerRows = 0;
+            int maxSum = int.MinValue;
+
+            for (int row = 0; row < rows - 2; row++)
+            {
+                for (int col = 0; col < cols - 2; col++)
+                {
+
+                    int currSum = numMatrix[row, col]
+                            + numMatrix[row + 1, col]
+                            + numMatrix[row + 2, col]
+                            + numMatrix[row, col + 1]
+                            + numMatrix[row, col + 2]
+                            + numMatrix[row + 1, col + 1]
+                            + numMatrix[row + 1, col + 2]
+                            + numMatrix[row + 2, col + 1]
+                            + numMatrix[row + 2, col + 2];
+
+                    if (currSum > maxSum)
+                    {
+                        maxSum = currSum;
+                        innerCols = col;
+                        innerRows = row;
+                    }
+                }
+            }
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int row = innerRows; row < innerRows + 3; row++)
+            {
+                for (int col = innerCols; col < innerCols + 3; col++)
+                {
+                    Console.Write(numMatrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 03.AdvancedCSharp/06.Ex.MultidimensionalArrays/05.SnakeMoves/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+namespace _05.SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = dimentions[0];
+            int cols = dimentions[1];
+            char[,] charMatrix = new char[rows, cols];
+            char[] snake = Console.ReadLine().ToCharArray();
+            int indexOfSnake = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 != 0)
+                {
+                    for (int col = cols - 1; col >= 0; col--)
+                    {
+                        SnakePath(charMatrix, snake, ref indexOfSnake, row, col);
+                    }
+                }
+                else
+                {
+                    for (int col = 0; col < cols; col++)
+                    {
+                        SnakePath(charMatrix, snake, ref indexOfSnake, row, col);
+                    }
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(charMatrix[row,col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void SnakePath(char[,] charMatrix, char[] snake, ref int indexOfSnake, int row, int col)
+        {
+            charMatrix[row, col] = snake[indexOfSnake];
+            indexOfSnake++;
+            if (indexOfSnake == snake.Length)
+            {
+                indexOfSnake = 0;
+            }
+
+        }
+    }
+}
