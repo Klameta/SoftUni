b170b0e97469b5b5f9236f166480625362f80938
Klameta <ivanavalcheva@gmail.com>
2022-09-24 16:13:52 +0300

    01.01 done

---
+++ 03.AdvancedCSharp/07.SetsAndDictionariesAdvanced/01.01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace _01._01.CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double, int> valuesCount = new Dictionary<double, int>();
+            double[] cmdArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            for (int i = 0; i < cmdArgs.Length; i++)
+            {
+                if (!valuesCount.ContainsKey(cmdArgs[i]))
+                {
+                    valuesCount.Add(cmdArgs[i], 1);
+                }
+                else
+                {
+                    valuesCount[cmdArgs[i]]++;
+                }
+            }
+
+            foreach (var value in valuesCount)
+            {
+                Console.WriteLine($"{value.Key} - {value.Value} times");
+            }
+        }
+    }
+}
