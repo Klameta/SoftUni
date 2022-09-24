a0ac508388309aeab938f5db0bd274811bc38265
Klameta <ivanavalcheva@gmail.com>
2022-09-24 17:45:11 +0300

    02.07 done

---
+++ 03.AdvancedCSharp/07.SetsAndDictionariesAdvanced/02.07.ParkingLot/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _02._07.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> carNumbers = new HashSet<string>();
+            string[] cmdArgs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (cmdArgs[0]!="END")
+            {
+                switch (cmdArgs[0])
+                {
+                    case "IN":
+                        carNumbers.Add(cmdArgs[1]);
+                        break;
+                    case "OUT":
+                        carNumbers.Remove(cmdArgs[1]);
+                        break;
+                }
+                cmdArgs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (carNumbers.Count()!= 0)
+            {
+                Console.WriteLine(string.Join("\n", carNumbers));
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
