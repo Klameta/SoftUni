1cbae9e7275538a921fd208f601d05952732f44e
Klameta <ivanavalcheva@gmail.com>
2022-09-24 19:03:11 +0300

    02 done

---
+++ 03.AdvancedCSharp/08.Ex.SetsAndDictionariesAdvanced/02.SetsOfElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nAndM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = nAndM[0];
+            int m = nAndM[1];
+            HashSet<int> firstNNums = new HashSet<int>();
+            HashSet<int> secondMNums = new HashSet<int>();
+
+            for (int i = 0; i < n+m; i++)
+            {
+                int currNum = int.Parse(Console.ReadLine());
+                if (i<n)
+                {
+                    firstNNums.Add(currNum);
+                }
+                else
+                {
+                    secondMNums.Add(currNum);
+                }
+            }
+
+            HashSet<int> repeating = new HashSet<int>();
+            foreach (var nNum in firstNNums)
+            {
+                if (secondMNums.Contains(nNum))
+                {
+                    repeating.Add(nNum);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", repeating));
+        }
+    }
+}
