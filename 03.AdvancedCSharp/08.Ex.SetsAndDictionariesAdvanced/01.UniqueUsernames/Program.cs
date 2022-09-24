f56f2cdf7f9f57ae6481dacc40a7076a6e11d5be
Klameta <ivanavalcheva@gmail.com>
2022-09-24 18:50:14 +0300

    01 done

---
+++ 03.AdvancedCSharp/08.Ex.SetsAndDictionariesAdvanced/01.UniqueUsernames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            HashSet<string> usernames = new HashSet<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                usernames.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join("\n", usernames));
+        }
+    }
+}
