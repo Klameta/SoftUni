8acafc0d21246e525ce821102ed6f2965c225c9b
Klameta <ivanavalcheva@gmail.com>
2022-09-24 16:33:55 +0300

    01.03 done

---
+++ 03.AdvancedCSharp/07.SetsAndDictionariesAdvanced/01.03Largest3Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _01._03Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // List<int> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            //Console.WriteLine(string.Join(" ", numbers.OrderByDescending(x => x).Take(3)));
+
+            Console.WriteLine(string.Join(" ", new List<int>(Console.ReadLine().Split().Select(int.Parse).ToList()).OrderByDescending(x => x).Take(3)));
+        }
+    }
+}
