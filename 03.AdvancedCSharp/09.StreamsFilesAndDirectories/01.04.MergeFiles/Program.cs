2d2f6a26ec857975123bc08926b5896c91c8feef
Klameta <ivanavalcheva@gmail.com>
2022-09-25 14:47:40 +0300

    01.04 done

--- 03.AdvancedCSharp/09.StreamsFilesAndDirectories/01.04.MergeFiles/Program.cs
+++ 03.AdvancedCSharp/09.StreamsFilesAndDirectories/01.04.MergeFiles/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace MergeFiles
+﻿using System.IO;
+using System;
+
+
+namespace MergeFiles
 {
     public class MergeFiles
     {
@@ -10,8 +14,42 @@
             MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
         }
         public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
-        { 
+        {
+            StreamReader input1 = new StreamReader(firstInputFilePath);
+            using (input1)
+            {
+                StreamReader input2 = new StreamReader(secondInputFilePath);
+                using (input2)
+                {
+                    StreamWriter writer = new StreamWriter(outputFilePath);
+                    using (writer)
+                    {
 
+                        while (!input1.EndOfStream & !input2.EndOfStream)
+                        {
+                            writer.WriteLine(input1.ReadLine());
+                            writer.WriteLine(input2.ReadLine());
+                        }
+
+                        if (input1.EndOfStream)
+                        {
+                            WriteExcess(input2, writer);
+                        }
+                        else if(input2.EndOfStream)
+                        {
+                            WriteExcess(input1, writer);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void WriteExcess(StreamReader input, StreamWriter writer)
+        {
+            while (!input.EndOfStream)
+            {
+                writer.WriteLine(input.ReadLine());
+            }
         }
     }
 }
