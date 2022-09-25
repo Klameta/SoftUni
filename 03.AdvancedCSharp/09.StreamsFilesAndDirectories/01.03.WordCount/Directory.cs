7547c10406f32261a2bcd6403f2bf413c762e76d
Klameta <ivanavalcheva@gmail.com>
2022-09-25 14:04:41 +0300

    01.03 done

---
+++ 03.AdvancedCSharp/09.StreamsFilesAndDirectories/01.03.WordCount/Directory.cs
@@ -0,0 +1,9 @@
+﻿namespace WordCount
+{
+    internal class Directory<T1, T2>
+    {
+        public Directory()
+        {
+        }
+    }
+}
---
+++ 03.AdvancedCSharp/09.StreamsFilesAndDirectories/01.04.MergeFiles/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MergeFiles
+{
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        { 
+
+        }
+    }
+}
