7357deb2ba3d0b34234fa1521c6cbff5c5f00d73
Klameta <ivanavalcheva@gmail.com>
2022-09-29 12:43:24 +0300

    forgot to push these. func programing 01,02,03,04

---
+++ 03.AdvancedCSharp/11.FunctionalPrograming/02.SumNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int count = nums.Length;
+            int sum = nums.Sum();
+            Console.WriteLine(count);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 03.AdvancedCSharp/11.FunctionalPrograming/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] upperCases = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.First() == x.ToUpper().First())
+                .ToArray();
+
+            Console.WriteLine(string.Join("\n", upperCases));
+        }
+    }
+}
