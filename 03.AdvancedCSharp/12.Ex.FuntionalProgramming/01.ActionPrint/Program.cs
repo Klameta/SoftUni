87415c1f1dc50a0a1b38c8ead7fe4bc9d6b85151
Klameta <ivanavalcheva@gmail.com>
2022-09-30 22:23:17 +0300

    01 done

---
+++ 03.AdvancedCSharp/12.Ex.FuntionalProgramming/01.ActionPrint/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+namespace _01.ActionPrint
+{
+    class Program
+    {
+        static Action<string[]> newLinePrint = x => Console.WriteLine(string.Join(Environment.NewLine, x));
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().
+                Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            newLinePrint(input);
+        }
+    }
+}
