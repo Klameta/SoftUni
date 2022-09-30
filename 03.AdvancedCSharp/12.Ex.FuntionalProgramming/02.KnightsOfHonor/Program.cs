4bac383616377e527d518fb428a3dd0263703607
Klameta <ivanavalcheva@gmail.com>
2022-09-30 22:32:39 +0300

    02 done

--- 03.AdvancedCSharp/12.Ex.FuntionalProgramming/02.KnightsOfHonor/Program.cs
+++ 03.AdvancedCSharp/12.Ex.FuntionalProgramming/02.KnightsOfHonor/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Linq;
 
 namespace _02.KnightsOfHonor
 {
     class Program
     {
+        static Action<string[]> knighting = x => Console.WriteLine(string.Join(Environment.NewLine, x.Select(x => $"Sir {x}").ToArray()));
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] names = Console.ReadLine().Split();
+
+            knighting(names);
         }
     }
 }
