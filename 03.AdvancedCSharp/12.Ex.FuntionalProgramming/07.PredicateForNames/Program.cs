369d3855c4375c7e767f32b0d6c3c1e7355dd123
Klameta <ivanavalcheva@gmail.com>
2022-10-02 22:41:35 +0300

    07, 08 done

---
+++ 03.AdvancedCSharp/12.Ex.FuntionalProgramming/07.PredicateForNames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, string, bool> lessThanFilter = (filter, name) => name.Length <= filter;
+
+            int filter = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => lessThanFilter(filter, x))
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
