6e1ba35f0ae5ca8e3daa0c046a7dc702f5b9ba60
Klameta <ivanavalcheva@gmail.com>
2022-10-02 21:35:33 +0300

    04 done

--- 03.AdvancedCSharp/12.Ex.FuntionalProgramming/04.FindEvensOrOdds/Program.cs
+++ 03.AdvancedCSharp/12.Ex.FuntionalProgramming/04.FindEvensOrOdds/Program.cs
@@ -1,13 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace _04.FindEvensOrOdds
 {
     class Program
     {
-        static Predicate<> oddOrEven = (nums, formater) =>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Predicate<int> even = (x) => x % 2 == 0;
+            Predicate<int> odd = (x) => x % 2 == 1 || x % 2 == -1;
+
+            int[] startEnd = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int start = startEnd[0];
+            int end = startEnd[1];
+            string filter = Console.ReadLine();
+            List<int> result = new List<int>();
+
+            for (int i = start; i <= end; i++)
+            {
+                if (filter == "odd" && odd(i))
+                {
+                    result.Add(i);
+                }
+                if (filter == "even" && even(i))
+                {
+                    result.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
         }
     }
 }
