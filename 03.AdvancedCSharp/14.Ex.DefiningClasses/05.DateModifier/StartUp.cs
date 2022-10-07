9a864bed5f3d58d4c35f85bc7c171f9bda28462c
Klameta <ivanavalcheva@gmail.com>
2022-10-07 12:08:34 +0300

    05 done

---
+++ 03.AdvancedCSharp/14.Ex.DefiningClasses/05.DateModifier/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] info = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            DateTime first = new DateTime(info[0], info[1], info[2]);
+
+            info = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            DateTime second = new DateTime(info[0], info[1], info[2]);
+            TimeSpan result = first - second;
+
+            Console.WriteLine(Math.Abs(result.Days));
+        }
+    }
+}
