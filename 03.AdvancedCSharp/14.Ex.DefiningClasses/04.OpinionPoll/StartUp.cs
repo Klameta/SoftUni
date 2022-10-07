5bbdd8fc235a7b9c6cda3cd5537c92b8b1766537
Klameta <ivanavalcheva@gmail.com>
2022-10-07 11:48:09 +0300

    04 done

---
+++ 03.AdvancedCSharp/14.Ex.DefiningClasses/04.OpinionPoll/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] personInfo = Console.ReadLine()
+                    .Split(' ',StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(personInfo[0], int.Parse(personInfo[1]));
+                people.Add(person);
+            }
+
+            people = people.Where(x => x.Age > 30)
+                .OrderBy(x => x.Name)
+                .ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, people));
+        }
+    }
+}
