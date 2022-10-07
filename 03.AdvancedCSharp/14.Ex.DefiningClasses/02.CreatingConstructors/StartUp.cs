eb970e101f46d23c291288feb94845f5e40ff1d2
Klameta <ivanavalcheva@gmail.com>
2022-10-07 11:27:46 +0300

    02 done, 03 almost

--- 03.AdvancedCSharp/14.Ex.DefiningClasses/02.CreatingConstructors/StartUp.cs
+++ 03.AdvancedCSharp/14.Ex.DefiningClasses/02.CreatingConstructors/StartUp.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 
-namespace _02.CreatingConstructors
+namespace DefiningClasses
 {
-    internal class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
---
+++ 03.AdvancedCSharp/14.Ex.DefiningClasses/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    internal class Family
+    {
+        public List<Person> People { get; set; }
+
+        public Family()
+        {
+            People = new List<Person>();
+        }
+
+        public void AddMember(Person person)
+        {
+            People.Add(person);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldest = new Person();
+            foreach (var member in People)
+            {
+                if (member.Age >= oldest.Age)
+                {
+                    oldest = member;
+                }
+            }
+            return oldest;
+        }
+    }
+}
