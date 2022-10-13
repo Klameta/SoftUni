e6b12919966beda8a20425888c725707cd0758b7
Klameta <ivanavalcheva@gmail.com>
2022-10-13 15:04:21 +0300

    07 done

---
+++ 03.AdvancedCSharp/18.Ex.Generics/07.Tuple/Tuple.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.Tuple
+{
+    public class Tuple<T, Y>
+    {
+        private T item1;
+        private Y item2;
+
+        public Tuple(T item1, Y item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+
+        public T Item1
+        {
+            get { return item1; }
+            set { item1 = value; }
+        }
+        public Y Item2
+        {
+            get { return item2; }
+            set { item2 = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{item1} -> {item2}";
+        }
+    }
+}
