133b872fa1ff9ef4bb63de1cb3125e27cb33da84
Klameta <ivanavalcheva@gmail.com>
2022-10-13 14:43:23 +0300

    05 done

---
+++ 03.AdvancedCSharp/18.Ex.Generics/05.GenericCountMethodString/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.GenericCountMethodString
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            Box<string> boxes = new Box<string>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                boxes.Items.Add(Console.ReadLine());
+            }
+            string compere = Console.ReadLine();
+
+            Console.WriteLine(boxes.CompareCount(compere));
+        }
+
+        
+    }
+}
