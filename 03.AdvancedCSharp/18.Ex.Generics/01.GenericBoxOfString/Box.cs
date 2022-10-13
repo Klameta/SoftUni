fcf431676d194dbf7185abf223fa7948c4edd687
Klameta <ivanavalcheva@gmail.com>
2022-10-13 13:01:58 +0300

    01 done

---
+++ 03.AdvancedCSharp/18.Ex.Generics/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private T item;
+
+        public Box(T item)
+        {
+            Item = item;
+        }
+
+        public T Item
+        {
+            get { return item; }
+            set { item = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{item.GetType().FullName}: {item}";
+        }
+    }
+}
---
+++ 03.AdvancedCSharp/18.Ex.Generics/01.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01.GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine());
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
