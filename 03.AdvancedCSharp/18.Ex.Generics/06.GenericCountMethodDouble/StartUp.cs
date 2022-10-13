270dbf6d210be36bf0cc726c089d9ae44a7a5608
Klameta <ivanavalcheva@gmail.com>
2022-10-13 14:45:29 +0300

    06 done

---
+++ 03.AdvancedCSharp/18.Ex.Generics/06.GenericCountMethodDouble/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.GenericCountMethodString
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            Box<double> boxes = new Box<double>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                boxes.Items.Add(double.Parse(Console.ReadLine()));
+            }
+            double compere = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(boxes.CompareCount(compere));
+        }
+
+        
+    }
+}
