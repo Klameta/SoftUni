7d284c6f6dec9e1994c6f06013157a7d079180f9
Klameta <ivanavalcheva@gmail.com>
2022-10-13 13:18:54 +0300

    02, 03 done

---
+++ 03.AdvancedCSharp/18.Ex.Generics/03.GenericSwapMethodString/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.GenericSwapMethodString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Box<string>> strings = new List<Box<string>>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                strings.Add(new Box<string>(Console.ReadLine()));
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Swap(strings, indexes[0], indexes[1]);
+
+            foreach (var item in strings)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Swap<T>(List<Box<T>> values, int firstIndex, int secondIndex)
+        {
+            (values[firstIndex], values[secondIndex]) = (values[secondIndex], values[firstIndex]);
+        }
+    }
+}
