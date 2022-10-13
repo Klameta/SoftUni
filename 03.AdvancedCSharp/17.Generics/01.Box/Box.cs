f2291992035605a38461939644ea3c87de062f36
Klameta <ivanavalcheva@gmail.com>
2022-10-13 12:43:07 +0300

    01,03 done

---
+++ 03.AdvancedCSharp/17.Generics/01.Box/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            Values = new List<T>();
+        }
+        public List<T> Values { get; set; }
+
+        public void Add(T element)
+        {
+            Values.Add(element);
+        }
+
+        public T Remove()
+        {
+            T result = Values[Values.Count - 1];
+            Values.Remove(result);
+
+            return result;
+        }
+
+        public int Count { get { return Values.Count; } }
+    }
+}
---
+++ 03.AdvancedCSharp/17.Generics/02.GenericScale/EqualityScale.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T left;
+        private T right;
+
+        public EqualityScale(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public bool AreEqual()
+        {
+            return Left.Equals(Right);
+        }
+
+        public T Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+
+        public T Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+
+
+    }
+}
