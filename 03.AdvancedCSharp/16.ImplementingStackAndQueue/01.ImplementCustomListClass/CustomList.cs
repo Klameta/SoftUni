f7ab92eb7ccf11ffa50175759df8c7cb3b97d06d
Klameta <ivanavalcheva@gmail.com>
2022-10-14 17:19:44 +0300

    01 started

---
+++ 03.AdvancedCSharp/16.ImplementingStackAndQueue/01.ImplementCustomListClass/CustomList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ImplementCustomListClass
+{
+    public class CustomList
+    {
+        int[] items ;
+        private const int InitialCapacity = 4;
+        private int lenght = InitialCapacity;
+        public int Count = 0;
+        public CustomList()
+        {
+            items = new int[InitialCapacity];
+        }
+
+        public void Add(int element)
+        {
+            if (Count + 1 >= lenght)
+            {
+                items = Resize();
+            }
+            items[Count + 1] = element;
+            Count++;
+        }
+
+        internal int[] Resize()
+        {
+            lenght *= 2;
+            int[] newArr = new int[lenght];
+            for (int i = 0; i < items.Length; i++)
+            {
+                newArr[i] = items[i];
+            }
+            return newArr;
+        }
+    }
+}
---
+++ 03.AdvancedCSharp/16.ImplementingStackAndQueue/01.ImplementCustomListClass/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.ImplementCustomListClass
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
