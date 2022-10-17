00bb0593e0722c11eefe43768e8b18db2ee47ca5
Klameta <ivanavalcheva@gmail.com>
2022-10-17 15:42:15 +0300

    01 done

---
+++ 03.AdvancedCSharp/19.IteratorsAndComparators/01.Library/Library.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
