1c89ab22c1618bbba8ef820b0c7a1b2a06fa2551
Klameta <ivanavalcheva@gmail.com>
2022-10-17 22:04:40 +0300

    04 done

---
+++ 03.AdvancedCSharp/20.Ex.IteratorsAndComparators/04.Froggy/Frog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Froggy
+{
+    public class Frog : IEnumerable<int>
+    {
+        private List<int> Pond;
+
+        public Frog(params int[] pond)
+        {
+            Pond = new List<int>(pond);
+        }
+
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < Pond.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return Pond[i];
+                }
+            }
+            bool even = Pond.Count % 2 == 0;
+
+            for (int i = Pond.Count - 1; i >= 0; i--)
+            {
+                if (i % 2 == 1)
+                {
+                    yield return Pond[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ 03.AdvancedCSharp/20.Ex.IteratorsAndComparators/04.Froggy/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] pond = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            Frog ints = new Frog(pond);
+            Console.WriteLine(string.Join(", ", ints));
+        }
+    }
+}
