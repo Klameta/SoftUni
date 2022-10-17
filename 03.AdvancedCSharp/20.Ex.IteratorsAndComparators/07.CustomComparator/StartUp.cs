a2af4fd0c1ace6da24ba8885a32407c8b41bd41b
Klameta <ivanavalcheva@gmail.com>
2022-10-17 22:28:44 +0300

    07 done

--- 03.AdvancedCSharp/20.Ex.IteratorsAndComparators/07.CustomComparator/StartUp.cs
+++ 03.AdvancedCSharp/20.Ex.IteratorsAndComparators/07.CustomComparator/StartUp.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace _07.CustomComparator
 {
     public class StartUp
     {
+        public class CustomComparator : IComparer<int>
+        {
+            public int Compare([AllowNull] int x, [AllowNull] int y)
+            {
+                if (x % 2 == 0 && Math.Abs(y) % 2 == 1)
+                    return -1;
+                if (Math.Abs(x) % 2 == 1 && y % 2 == 0)
+                    return 1;
+                return x.CompareTo(y);
+            }
+        }
         static void Main(string[] args)
         {
             int[] nums = Console.ReadLine()
