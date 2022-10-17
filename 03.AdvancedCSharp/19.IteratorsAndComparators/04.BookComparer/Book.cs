3c62d1f91959e48f095d20c0d02fee01a5b01de7
Klameta <ivanavalcheva@gmail.com>
2022-10-17 16:16:36 +0300

    04 done

---
+++ 03.AdvancedCSharp/19.IteratorsAndComparators/04.BookComparer/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = new List<string>(authors);
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public int CompareTo([AllowNull] Book other)
+        {
+            if (Year.CompareTo(other.Year)==0)
+            {
+                return Title.CompareTo(other.Title);
+            }
+            return Year.CompareTo(other.Year);
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+    }
+}
