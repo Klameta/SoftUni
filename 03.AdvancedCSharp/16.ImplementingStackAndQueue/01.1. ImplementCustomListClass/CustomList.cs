3fcdffc9a48ac2259204a22a2d36087b1fe9c964
Klameta <ivanavalcheva@gmail.com>
2022-10-17 15:21:20 +0300

    merging

---
+++ 03.AdvancedCSharp/16.ImplementingStackAndQueue/01.1. ImplementCustomListClass/CustomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._1._ImplementCustomListClass
+{
+    public class CustomList
+    {
+        private const int InitialCapacity = 2;
+        private int[] items;
+
+        public CustomList()
+        {
+            this.items = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+    }
+}
---
+++ 03.AdvancedCSharp/16.ImplementingStackAndQueue/01.1. ImplementCustomListClass/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_ImplementCustomListClass
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
