b8c0b3c607a9517321f6199073c9d79de5e9b1d6
Klameta <ivanavalcheva@gmail.com>
2022-10-18 23:56:59 +0300

    01 started

---
+++ 03.AdvancedCSharp/22.ExamPreperation/01.FoodFinded/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.FoodFinded
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<char> vowels = new Queue<char>();
+            Stack<char> consonants = new Stack<char>();
+
+            char[] inputVowels = Console.ReadLine()
+                                        .Split(' ')
+                                        .Select(char.Parse)
+                                        .ToArray();
+            char[] inputConsanats = Console.ReadLine()
+                                           .Split(' ')
+                                           .Select(char.Parse)
+                                           .ToArray();
+
+            foreach (var vowel in inputVowels)
+            {
+                vowels.Enqueue(vowel);
+            }
+            foreach (var constant in inputConsanats)
+            {
+                consonants.Push(constant);
+            }
+
+            Dictionary<string, List<char>> words = new Dictionary<string, List<char>>();
+
+            while (consonants.Count != 0)
+            {
+                var currVowel = vowels.Dequeue();
+                var currCons = consonants.Pop();
+
+
+            }
+        }
+    }
+}
