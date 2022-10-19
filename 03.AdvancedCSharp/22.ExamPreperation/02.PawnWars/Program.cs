6db025b9ee2cc231f03adbde9f3846abcf55aa12
Klameta <ivanavalcheva@gmail.com>
2022-10-19 08:45:48 +0300

    02 started

---
+++ 03.AdvancedCSharp/22.ExamPreperation/02.PawnWars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02.PawnWars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] chessBoard = new char[8, 8];
+
+            for (int row = 0; row < 8; row++)
+            {
+                char[] inputChess = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < 8; col++)
+                {
+                    chessBoard[row,col] = inputChess[col];
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
