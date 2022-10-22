192720ed63629c2d8fc0c585c8f9492edf33f190
Klameta <ivanavalcheva@gmail.com>
2022-10-22 15:43:44 +0300

    exam DONE

---
+++ 03.AdvancedCSharp/24.RegularExam/01EnergyDrinks/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01EnergyDrinks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> mgCaffeine = new Stack<int>();
+            Queue<int> energyDrinks = new Queue<int>();
+
+            int[] numsFirst = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] numsSecond = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < numsFirst.Length; i++)
+            {
+                mgCaffeine.Push(numsFirst[i]);
+            }
+            for (int i = 0; i < numsSecond.Length; i++)
+            {
+                energyDrinks.Enqueue(numsSecond[i]);
+            }
+
+            int totalCaffeine = 0;
+            int max = 300;
+
+
+            while (true)
+            {
+                if (mgCaffeine.Count == 0 || energyDrinks.Count == 0) //break
+                {
+                    break;
+                }
+
+                int currDrink = mgCaffeine.Peek() * energyDrinks.Peek();
+
+                if (currDrink + totalCaffeine <= max)
+                {
+                    totalCaffeine += currDrink;
+                    mgCaffeine.Pop();
+                    energyDrinks.Dequeue();
+                }
+                else
+                {
+                    mgCaffeine.Pop();
+                    var temp = energyDrinks.Dequeue();
+                    energyDrinks.Enqueue(temp);
+                    if (totalCaffeine >= 30)
+                    {
+                        totalCaffeine -= 30;
+                    }
+                }
+
+            }
+
+            if (energyDrinks.Count>0)
+            {
+                Console.WriteLine($"Drinks left: {string.Join(", ", energyDrinks)}");
+            }
+            else
+            {
+                Console.WriteLine("At least Stamat wasn't exceeding the maximum caffeine.");
+            }
+
+            Console.WriteLine($"Stamat is going to sleep with {totalCaffeine} mg caffeine.");
+        }
+    }
+}
---
+++ 03.AdvancedCSharp/24.RegularExam/03.ComputerArchitecture/CPU.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerArchitecture
+{
+    public class CPU
+    {
+        private string brand;
+        private int cores;
+        private double frequency;
+
+        public CPU(string brand, int cores, double frequency)
+        {
+            Brand = brand;
+            Cores = cores;
+            Frequency = frequency;
+        }
+
+        public override string ToString()
+        {
+            return $"{brand} CPU:" +
+                $"\nCores: {cores}" +
+                $"\nFrequency: {frequency:F1} GHz";
+        }
+
+        public string Brand
+        {
+            get { return brand; }
+            private set { brand = value; }
+        }
+        public int Cores
+        {
+            get { return cores; }
+            private set { cores = value; }
+        }
+        public double Frequency
+        {
+            get { return frequency; }
+            private set { frequency = value; }
+        }
+
+    }
+
+}
