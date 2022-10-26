b0df1f1e97c0e880b69b16cec2726f03ec54c3d2
Klameta <ivanavalcheva@gmail.com>
2022-10-26 16:18:25 +0300

    04 done

---
+++ 03.AdvancedCSharp/02.C# OOP/01.Inheritance/04.RandomList/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            RandomList strings = new RandomList();
+            strings.Add("sad");
+            strings.Add("hyhf");
+            strings.Add("sgf");
+            Console.WriteLine(strings.RandomString());
+        }
+    }
+}
