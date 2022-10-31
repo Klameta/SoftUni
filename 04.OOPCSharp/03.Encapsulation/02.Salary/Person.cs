da35177ecbd654ae2597ea6b0cc52013e017843b
Klameta <ivanavalcheva@gmail.com>
2022-10-31 16:15:27 +0200

    01, 02 done

---
+++ 04.OOPCSharp/03.Encapsulation/02.Salary/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                Salary += Salary * (percentage * 0.01M / 2);
+            }
+            else
+            {
+                Salary +=  Salary*percentage*0.01M;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+    }
+}
