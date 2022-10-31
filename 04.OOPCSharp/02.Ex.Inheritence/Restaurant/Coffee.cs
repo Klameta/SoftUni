9fec36e4f2471065a4546c37f10fc4ed76a2a73e
Klameta <ivanavalcheva@gmail.com>
2022-10-31 12:58:50 +0200

    05 done

---
+++ 04.OOPCSharp/02.Ex.Inheritence/Restaurant/Coffee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        public Coffee(string name, double caffeine) : base(name, 0, 0)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double CoffeeMilliliters { get { return 50; } }
+        public decimal CoffeePrice { get { return 3.50M; } }
+        public double Caffeine { get; set; }
+
+        public override decimal Price { get { return CoffeePrice; } }
+        public override double Milliliters { get { return CoffeeMilliliters; } }
+    }
+}
---
+++ 04.OOPCSharp/02.Ex.Inheritence/Restaurant/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Product
+    {
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+       virtual public string Name { get; set; }
+       virtual public decimal Price { get; set; }
+
+    }
+}
