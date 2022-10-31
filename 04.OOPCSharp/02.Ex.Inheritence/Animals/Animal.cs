31d2ef5d7a8d4356b4eabea12ebd6b9b413c4254
Klameta <ivanavalcheva@gmail.com>
2022-10-31 14:18:40 +0200

    06 started

---
+++ 04.OOPCSharp/02.Ex.Inheritence/Animals/Animal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == null) throw new ArgumentException("Invalid input!");
+                name = value;
+
+            }
+        }
+        public virtual int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value <= 0) throw new ArgumentException("Invalid input!");
+                age = value;
+
+            }
+        }
+        public string Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (value == null) throw new ArgumentException("Invalid input!");
+                gender = value;
+
+            }
+        }
+
+        public virtual string ProduceSound()=> null;
+        public override string ToString()
+        {
+            return $"{GetType().Name}" +
+                $"\n{Name} {Age} {Gender}" +
+                $"\n{ProduceSound()}";
+        }
+    }
+}
--- 04.OOPCSharp/02.Ex.Inheritence/Animals/StartUp.cs
+++ 04.OOPCSharp/02.Ex.Inheritence/Animals/StartUp.cs
@@ -1,10 +1,59 @@
-﻿namespace Animals
+﻿using System;
+using System.Collections.Generic;
+namespace Animals
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            string[] cmdArgs = Console.ReadLine().Split();
+            List<Animal> animals = new List<Animal>();
+
+            while (cmdArgs[0] != "Beast!")
+            {
+                string type = cmdArgs[0];
+
+                cmdArgs = Console.ReadLine().Split();
+
+                string name = cmdArgs[0];
+                int age = int.Parse(cmdArgs[1]);
+                string gender = cmdArgs[2];
+
+                Animal animal = null;
+                try
+                {
+
+                    switch (type)
+                    {
+                        case "Cat":
+                            animal = new Cat(name, age, gender);
+                            break;
+                        case "Kitten":
+                            animal = new Kitten(name, age, gender);
+                            break;
+                        case "Tomcat":
+                            animal = new Tomcat(name, age, gender);
+                            break;
+                        case "Dog":
+                            animal = new Dog(name, age, gender);
+                            break;
+                        case "Frog":
+                            animal = new Frog(name, age, gender);
+                            break;
+                    }
+                    animals.Add(animal);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                cmdArgs = Console.ReadLine().Split();
+            }
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal.ToString());
+            }
         }
     }
 }
