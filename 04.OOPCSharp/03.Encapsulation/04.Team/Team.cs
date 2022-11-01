9b93fd88b2dafb591a609092891aa8cb92622c54
Klameta <ivanavalcheva@gmail.com>
2022-11-01 17:27:53 +0200

    03, 04 done

---
+++ 04.OOPCSharp/03.Encapsulation/04.Team/Team.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"First team has {firstTeam.Count} players." +
+                $"\nReserve team has {reserveTeam.Count} players.";
+        }
+
+        readonly LinkedList<Person> FirstTeam => FirstTeam;
+        readonly List<Person> ReserveTeam => reserveTeam;
+        public string Name { get; private set; }
+    }
+}
