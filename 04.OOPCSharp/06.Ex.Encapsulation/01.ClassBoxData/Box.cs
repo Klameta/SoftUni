be8c33dc792841b0b321cadbcdf3d7d6785e24ba
Klameta <ivanavalcheva@gmail.com>
2022-11-05 14:32:13 +0200

    01 done

---
+++ 04.OOPCSharp/06.Ex.Encapsulation/01.ClassBoxData/Box.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        private double SurfaceArea()
+        {
+            return 2 * (length * width + length * height + width * height);
+        }
+        private double LateralSurfaceArea()
+        {
+            return 2 * height * (length + width);
+        }
+        private double Volume()
+        {
+            return length * width * height;
+        }
+
+        public override string ToString()
+        {
+            return $"Surface Area - {SurfaceArea():F2}\n" +
+                $"Lateral Surface Area - {LateralSurfaceArea():F2}\n" +
+                $"Volume - {Volume():F2}";
+        }
+
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if (value <= 0) throw new ArgumentException($"Length cannot be zero or negative.");
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                if (value <= 0) throw new ArgumentException($"Width cannot be zero or negative.");
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set
+            {
+                if (value <= 0) throw new ArgumentException($"Height cannot be zero or negative.");
+                height = value;
+            }
+        }
+
+
+    }
+}
---
+++ 04.OOPCSharp/06.Ex.Encapsulation/01.ClassBoxData/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.ClassBoxData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(lenght, width, height);
+
+                Console.WriteLine(box.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
