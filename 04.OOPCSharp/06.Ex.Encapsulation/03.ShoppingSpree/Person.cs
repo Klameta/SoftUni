3789aa9cc79ede213e183387da4148db4cf322cf
Klameta <ivanavalcheva@gmail.com>
2022-11-06 13:43:14 +0200

    03 started

---
+++ 04.OOPCSharp/06.Ex.Encapsulation/03.ShoppingSpree/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03.ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bag;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            bag = new List<Product>();
+        }
+
+        public void Purchase(Product product)
+        {
+            if (money >= product.Cost)
+            {
+                money -= product.Cost;
+                bag.Add(product);
+
+                Console.WriteLine($"{Name} bought {product.Name}");
+
+                return;
+            }
+
+            Console.WriteLine($"{Name} can't afford {product.Name}");
+        }
+
+        public override string ToString()
+        {
+            if (bag.Count > 0) return $"{Name} - {string.Join(", ", bag.Select(x => x.Name))}";
+
+            return $"{Name} - Nothing bought";
+        }
+
+        public IReadOnlyCollection<Product> Bag { get { return bag.AsReadOnly(); } }
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value)) throw new ArgumentException("Name cannot be empty");
+
+                name = value;
+            }
+        }
+        public decimal Money
+        {
+            get { return money; }
+            private set
+            {
+                if (value < 0) throw new ArgumentException("Money cannot be negative");
+                money = value;
+            }
+        }
+    }
+
+}
---
+++ 04.OOPCSharp/06.Ex.Encapsulation/03.ShoppingSpree/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.ShoppingSpree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            List<Product> products = new List<Product>();
+
+            List<string> peopleInfo = Console.ReadLine()
+                .Split(";", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            List<string> productInfo = Console.ReadLine()
+                .Split(";", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            try
+            {
+                Reading(people, products, peopleInfo, productInfo);
+
+                string[] cmdArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                while (cmdArgs[0] != "END")
+                {
+                    string personName = cmdArgs[0];
+                    string productName = cmdArgs[1];
+
+                    Person person = people.FirstOrDefault(x => x.Name == personName);
+                    Product product = products.FirstOrDefault(x => x.Name == productName);
+
+                    if (person != null && product != null)
+                    person.Purchase(product);
+
+                    cmdArgs = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+
+        private static void Reading(List<Person> people, List<Product> products, List<string> peopleInfo, List<string> productInfo)
+        {
+            foreach (var per in peopleInfo)
+            {
+                string[] currPerson = per.Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(currPerson[0], decimal.Parse(currPerson[1]));
+                people.Add(person);
+            }
+
+            foreach (var product in productInfo)
+            {
+                string[] currProduct = product.Split("=", StringSplitOptions.RemoveEmptyEntries);
+
+                Product product1 = new Product(currProduct[0], decimal.Parse(currProduct[1]));
+                products.Add(product1);
+            }
+        }
+    }
+}
