25c2d72db9dab0809b009264cfddcadca434d815
Klameta <ivanavalcheva@gmail.com>
2022-11-11 23:31:06 +0200

    01, 02 done

---
+++ 04.OOPCSharp/09.Polymorhpism/01.MathOperations/MathOperations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int first, int second) => first + second;
+        public double Add(double first, double second, double third) =>  first + second + third;
+        public decimal Add(decimal first, decimal second, decimal third) => first + second + third;
+
+    }
+}
