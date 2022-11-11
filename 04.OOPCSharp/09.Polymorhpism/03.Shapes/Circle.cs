9ed71b504f1b35c204cc1463f945ffdfdb113dd1
Klameta <ivanavalcheva@gmail.com>
2022-11-11 23:56:05 +0200

    03 done 09 DONE

---
+++ 04.OOPCSharp/09.Polymorhpism/03.Shapes/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Math.PI * 2 * radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+
+
+    }
+}
---
+++ 04.OOPCSharp/09.Polymorhpism/03.Shapes/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(3);
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.CalculateArea());
+
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
