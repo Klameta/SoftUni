5a9219982e5d4cd0db7b2035351783de9a7a5cde
Klameta <ivanavalcheva@gmail.com>
2022-11-12 17:36:13 +0200

    02 done

---
+++ 04.OOPCSharp/10.Ex.Polymorphsm/02.VehiclesExtension/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.VehiclesExtension
+{
+    public class Car : Vehicle
+    {
+        private const double CONSUMPTION_INCREASE = 0.9;
+
+        public Car(double fuelQuantity, double fuelConsumption, double tankCapacity)
+            : base(fuelQuantity, fuelConsumption + CONSUMPTION_INCREASE, tankCapacity)
+        { }
+    }
+}
---
+++ 04.OOPCSharp/10.Ex.Polymorphsm/02.VehiclesExtension/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+namespace _02.VehiclesExtension
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] carInfo = Console.ReadLine().Split(' ');
+            Car car = new Car(double.Parse(carInfo[1]), double.Parse(carInfo[2]), double.Parse(carInfo[3]));
+
+            string[] truckInfo = Console.ReadLine().Split();
+            Truck truck = new Truck(double.Parse(truckInfo[1]), double.Parse(truckInfo[2]), double.Parse(truckInfo[3]));
+
+            string[] busInfo = Console.ReadLine().Split();
+            Bus bus = new Bus(double.Parse(busInfo[1]), double.Parse(busInfo[2]), double.Parse(busInfo[3]));
+
+
+
+            int lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split();
+                double amount = double.Parse(cmdArgs[2]);
+                switch (cmdArgs[0])
+                {
+                    case "Refuel":
+                        if (cmdArgs[1] == "Car")
+                        {
+                            car.Refuel(amount);
+                        }
+                        else if (cmdArgs[1] == "Truck")
+                        {
+                            truck.Refuel(amount);
+                        }
+                        else
+                        {
+                            bus.Refuel(amount);
+                        }
+                        break;
+                    case "Drive":
+                        if (cmdArgs[1] == "Car")
+                        {
+                            car.Drive(amount);
+                        }
+                        else if (cmdArgs[1] == "Truck")
+                        {
+                            truck.Drive(amount);
+                        }
+                        else
+                        {
+                            bus.Drive(amount);
+                        }
+                        break;
+                    default:
+                        bus.DriveEmpty(amount);
+                        break;
+                }
+            }
+            Console.WriteLine(car);
+            Console.WriteLine(truck);
+            Console.WriteLine(bus);
+        }
+    }
+}
