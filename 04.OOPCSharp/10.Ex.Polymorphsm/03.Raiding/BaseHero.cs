863e051997e9acb2c2b671906ea43c745ff29240
Klameta <ivanavalcheva@gmail.com>
2022-11-12 18:12:10 +0200

    03 done

---
+++ 04.OOPCSharp/10.Ex.Polymorphsm/03.Raiding/BaseHero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Raiding
+{
+    public class BaseHero
+    {
+        public BaseHero(string name, int power)
+        {
+            Name = name;
+            Power = power;
+        }
+
+        public virtual string CastAbility()
+        {
+            return $"{GetType().Name} - {Name} {(GetType().Name == "Druid" || GetType().Name == "Paladin" ? "healed" : "hit")} for {Power}{(GetType().Name == "Druid" || GetType().Name == "Paladin" ? "" : " damage")}";
+        }
+
+        public string Name { get; private set; }
+        public int Power { get; private set; }
+
+    }
+}
---
+++ 04.OOPCSharp/10.Ex.Polymorphsm/03.Raiding/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.Raiding
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<BaseHero> heroes = new List<BaseHero>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string name = Console.ReadLine();
+                string type = Console.ReadLine();
+
+                BaseHero hero= null;
+                switch (type)
+                {
+                    case "Druid":
+                        hero = new Druid(name);
+                        break;
+                    case "Paladin":
+                        hero = new Paladin(name);
+                        break;
+                    case "Rogue":
+                        hero = new Rogue(name);
+                        break;
+                    case "Warrior":
+                        hero = new Warrior(name);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid hero!");
+                        break;
+                }
+
+                if (hero != null) heroes.Add(hero);
+                else
+                {
+                    i--;
+                }
+            }
+
+            int bbegPower = int.Parse(Console.ReadLine());
+            int partyPower = 0;
+
+            foreach (var hero in heroes)
+            {
+                Console.WriteLine(hero.CastAbility());
+                partyPower += hero.Power;
+            }
+
+            if (bbegPower<=partyPower)
+            {
+                Console.WriteLine("Victory!");
+            }
+            else
+            {
+                Console.WriteLine("Defeat...");
+            }
+        }
+    }
+}
