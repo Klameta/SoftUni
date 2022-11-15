ad38365c6d167a7381fd1ca6702fe1b992420a48
Klameta <ivanavalcheva@gmail.com>
2022-11-15 16:56:49 +0200

    04, 05 done

--- 04.OOPCSharp/11.ExceptionHandling/04.SumОfIntegers/Program.cs
+++ 04.OOPCSharp/11.ExceptionHandling/04.SumОfIntegers/Program.cs
@@ -10,26 +10,31 @@ namespace _04.SumОfIntegers
             string[] nums = Console.ReadLine().Split();
             int sum = 0;
             string currElement = "";
-            try
+            int currNum = 0;
+            foreach (var num in nums)
             {
-                int currNum = 0;
-                foreach (var num in nums)
+                try
                 {
                     currElement = num;
                     currNum = int.Parse(num);
+                    sum+=currNum;
                 }
-            }
-            catch (Exception ex)
-            {
-                if (ex.GetType().Name == "FormatException")
+                catch (Exception ex)
                 {
-                    Console.WriteLine($"The element '{currElement}' is in wrong format!");
-                }
-                else if (ex.GetType().Name == "OverflowException")
-                {
-                    Console.WriteLine($"The element '{currElement}' is out of range!");
-                }
+                    if (ex.GetType().Name == "FormatException")
+                    {
+                        Console.WriteLine($"The element '{currElement}' is in wrong format!");
+                    }
+                    else if (ex.GetType().Name == "OverflowException")
+                    {
+                        Console.WriteLine($"The element '{currElement}' is out of range!");
+                    }
 
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{currElement}' processed - current sum: {sum}");
+                }
             }
 
             Console.WriteLine($"The total sum of all integers is: {sum}");
---
+++ 04.OOPCSharp/11.ExceptionHandling/05.PlayCatch/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.PlayCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int exceptions = 0;
+            while (exceptions < 3)
+            {
+                try
+                {
+                    string[] cmdArgs = Console.ReadLine().Split();
+
+                    switch (cmdArgs[0])
+                    {
+                        case "Replace":
+                            int index = int.Parse(cmdArgs[1]);
+                            int element = int.Parse(cmdArgs[2]);
+
+                            nums[index] = element;
+                            break;
+                        case "Print":
+                            int startIndex = int.Parse(cmdArgs[1]);
+                            int endIndex = int.Parse(cmdArgs[2]);
+
+                            if (startIndex<0||endIndex>=nums.Count)
+                            {
+                                throw new ArgumentOutOfRangeException();
+                            }
+
+                            Console.WriteLine(String.Join(", ", nums
+                                .Skip(startIndex)
+                                .Take(endIndex - startIndex + 1)));
+                            break;
+                        case "Show":
+                            index = int.Parse(cmdArgs[1]);
+                            Console.WriteLine(nums[index]);
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    switch (ex.GetType().Name)
+                    {
+                        case "ArgumentOutOfRangeException":
+                            Console.WriteLine("The index does not exist!");
+                            break;
+                        case "FormatException":
+                            Console.WriteLine("The variable is not in the correct format!");
+                            break;
+                    }
+                    exceptions++;
+                }
+
+            }
+                Console.WriteLine(String.Join(", ", nums));
+
+        }
+    }
+}
