c99643df99bb26eed524a2ad281fd983370f385f
Klameta <ivanavalcheva@gmail.com>
2022-11-24 10:42:30 +0200

    01 started

---
+++ 03.AdvancedCSharp/02.C# OOP/13.ReflectionAndAttributes/01.Stealer/Spy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string name, string[] fieldsNames)
+        {
+            Type type = Type.GetType(name);
+
+            return $"Class under investigation: {type.Name}";
+        }
+    }
+}
