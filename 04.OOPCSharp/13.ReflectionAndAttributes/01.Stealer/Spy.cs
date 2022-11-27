786a365aab9f40f957eacb7f1197c8394f288b1a
Klameta <ivanavalcheva@gmail.com>
2022-11-27 11:41:51 +0200

    Revert "01 started"
    This reverts commit c1df4241d4e03539dce3dc3bcef454d91b125e7d.
    
    # Conflicts:
    #	04.OOPCSharp/02.Ex.Inheritence/Animals/06.Animals.csproj
    #	04.OOPCSharp/02.Ex.Inheritence/InheritanceExercise.sln
    #	04.OOPCSharp/02.Ex.Inheritence/NeedForSpeed/04.NeedForSpeed.csproj
    #	04.OOPCSharp/02.Ex.Inheritence/NeedForSpeed/StartUp.cs
    #	04.OOPCSharp/02.Ex.Inheritence/Person/01.Person.csproj
    #	04.OOPCSharp/02.Ex.Inheritence/Person/Child.cs
    #	04.OOPCSharp/02.Ex.Inheritence/Person/Person.cs
    #	04.OOPCSharp/02.Ex.Inheritence/Person/StartUp.cs
    #	04.OOPCSharp/02.Ex.Inheritence/PlayersAndMonsters/03.PlayersAndMonsters.csproj
    #	04.OOPCSharp/02.Ex.Inheritence/PlayersAndMonsters/StartUp.cs
    #	04.OOPCSharp/02.Ex.Inheritence/Restaurant/05.Restaurant.csproj
    #	04.OOPCSharp/02.Ex.Inheritence/Restaurant/StartUp.cs
    #	04.OOPCSharp/02.Ex.Inheritence/Zoo/02.Zoo.csproj
    #	04.OOPCSharp/02.Ex.Inheritence/Zoo/StartUp.cs

---
+++ 04.OOPCSharp/13.ReflectionAndAttributes/01.Stealer/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string name, string[] fieldsNames)
+        {
+            Type type = Type.GetType(name);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"Class under investigation: {type.Name}");
+
+            object instance = Activator.CreateInstance(type, new object[] { });
+
+            var classFields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static | BindingFlags.NonPublic);
+
+            foreach (var classField in classFields.Where(f => fieldsNames.Contains(f.Name)))
+            {
+                stringBuilder.AppendLine($"{classField.Name} = {classField.GetValue(instance)}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
--- 04.OOPCSharp/13.ReflectionAndAttributes/01.Stealer/StartUp.cs
+++ 04.OOPCSharp/13.ReflectionAndAttributes/01.Stealer/StartUp.cs
@@ -7,7 +7,7 @@ namespace Stealer
         static void Main(string[] args)
         {
             Spy spy = new Spy();
-            string result = spy.StealFieldInfo("Stealer.Hacker", new string[] { "" });
+            string result = spy.StealFieldInfo("Stealer.Hacker", new string[] { "username", "password" });
             Console.WriteLine(result);
         }
     }
