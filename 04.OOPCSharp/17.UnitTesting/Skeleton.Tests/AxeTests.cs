5466d74d1141c896cbca23184eb2c7fdcc64e7db
Klameta <ivanavalcheva@gmail.com>
2022-12-05 23:52:08 +0200

    01 done

---
+++ 04.OOPCSharp/17.UnitTesting/Skeleton.Tests/AxeTests.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        Axe axe;
+        Dummy dummy;
+
+        [SetUp]
+        public void SetUp()
+        {
+            axe = new Axe(10, 5);
+            dummy = new Dummy(100, 5);
+        }
+
+        [Test]
+        public void WeaponLossingDurablityWhenAttacking()
+        {
+            axe.Attack(dummy);
+
+            Assert.AreEqual(4, axe.DurabilityPoints);
+        }
+
+        [Test]
+        public void WeaponCannotAttackWhenBroken()
+        {
+            axe = new Axe(0, 0);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                axe.Attack(dummy);
+            }
+            );
+        }
+    }
+}
