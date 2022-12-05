e0cb0baae0f5203bbe4388840e0237e7bad581b9
Klameta <ivanavalcheva@gmail.com>
2022-12-06 00:03:43 +0200

    02 done 17 DONE

--- 04.OOPCSharp/17.UnitTesting/Skeleton.Tests/AxeTests.cs
+++ 04.OOPCSharp/17.UnitTesting/Skeleton.Tests/AxeTests.cs
@@ -35,5 +35,7 @@ namespace Skeleton.Tests
             }
             );
         }
+
+
     }
 }
--- 04.OOPCSharp/17.UnitTesting/Skeleton.Tests/DummyTests.cs
+++ 04.OOPCSharp/17.UnitTesting/Skeleton.Tests/DummyTests.cs
@@ -1,14 +1,60 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
     [TestFixture]
     public class DummyTests
     {
-        [Test]
-        public void Test1()
-        {
 
+        Dummy dummy;
+        Axe axe;
+
+        [SetUp]
+        public void SetUp()
+        {
+            dummy = new Dummy(10, 5);
+            axe = new Axe(5, 5);
+        }
+        [Test]
+        public void DummyShouldLoseHealthWhenAttacked()
+        {
+            axe.Attack(dummy);
+
+            Assert.AreEqual(5, dummy.Health);
+        }
+
+        [Test]
+        public void DeadDummyShouldThrowExceptionWhenAttackedDead()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    axe.Attack(dummy);
+                }
+
+            });
+        }
+
+        [Test]
+        public void DeadDummyShouldGiveXP()
+        {
+            axe.Attack(dummy);
+            axe.Attack(dummy);
+
+            int xp = dummy.GiveExperience();
+
+            Assert.AreEqual(5, xp);
+        }
+
+        [Test]
+        public void AliveDummyShouldNotGiveXP()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                dummy.GiveExperience();
+            });
         }
     }
 }
