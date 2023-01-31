50fb1cc616e57f7b95d9809d03e053fb56f0cca7
Klameta <ivanavalcheva@gmail.com>
2023-02-01 00:43:18 +0200

    done

--- 05.MSSQL/05.Subqueries and Joins/01-17.sql
+++ 05.MSSQL/05.Subqueries and Joins/01-17.sql
@@ -82,10 +82,94 @@ where m.EmployeeID in (3,7)
 order by e.EmployeeID
 
 -- 10
+use SoftUni
+select top(50) e.EmployeeID, e.FirstName + ' ' + e.LastName as EmployeeName
+             , m.FirstName + ' ' + m.LastName as ManagerName
+             , d.Name as DepartmentName
+from Employees as e
+join Employees as m
+    on e.ManagerID = m.EmployeeID
+join Departments d
+    on d.DepartmentID = e.DepartmentID
+order by e.EmployeeID
+
+-- 11
+select min(a.AverageSalary) as MinAverageSalary
+from
+(
+    select avg(e.Salary) as AverageSalary
+    from Employees as e
+    group by e.DepartmentID
+
+) as a
+
+-- 12
+use Geography
+
+select c.CountryCode, m.MountainRange, p.PeakName, p.Elevation
+from Peaks as p
+join Mountains as m
+    on p.MountainId = m.Id
+join MountainsCountries MC
+    on m.Id = MC.MountainId
+join Countries C
+    on C.CountryCode = MC.CountryCode
+where c.CountryName = 'Bulgaria'
+    and p.Elevation > 2835
+order by p.Elevation desc
+
+-- 13
+
+select c.CountryCode, count(m.Id) as MountainRanges
+from Countries as c
+join MountainsCountries MC
+    on c.CountryCode = MC.CountryCode
+join Mountains as m
+    on MC.MountainId = m.Id
+where c.CountryCode in ('BG', 'RU', 'US')
+group by c.CountryCode
+
+-- 14
+
+select top(5) c.CountryName, r.RiverName
+from Countries as c
+left join CountriesRivers as cr
+    on c.CountryCode = cr.CountryCode
+left join Rivers R
+    on cr.RiverId = R.Id
+where c.ContinentCode = 'AF'
+order by c.CountryName
+
+-- 16
+select count(c.CountryCode) as 'Count'
+from Countries as c
+left join MountainsCountries as mc
+    on c.CountryCode = mc.CountryCode
+where mc.CountryCode is null
+
+-- 17
+select top(5)c.CountryName,
+       max(p.Elevation) as HighestPeakElevation,
+       max(r.Length) as LongestRiverLength
+from Countries as c
+join MountainsCountries MC
+    on c.CountryCode = MC.CountryCode
+join Mountains M
+    on M.Id = MC.MountainId
+join Peaks P
+    on M.Id = P.MountainId
+join CountriesRivers CR
+    on c.CountryCode = CR.CountryCode
+join Rivers R
+    on R.Id = CR.RiverId
+group by c.CountryName
+order by HighestPeakElevation desc,
+         LongestRiverLength desc
 
 
 
-
+select *
+from ;
 
 
 
