912b0bd06e5084d9047d7082d8290b5c50ee7c18
Klameta <ivanavalcheva@gmail.com>
2023-06-13 19:26:34 +0300

    MVC 2 DONE

---
+++ 07.ASP.NetFundamentals/01.ASP.NetIntro/ChatApp/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,39 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private  List<KeyValuePair<string, string>> _messages =
+             new List<KeyValuePair<string, string>>();
+
+        public IActionResult Show()
+        {
+            if (_messages.Count() < 1)
+            {
+                return View(new ChatViewModel());
+            }
+
+            var chatModel = new ChatViewModel()
+            {
+                Messages = _messages
+                    .Select(m => new MessageViewModel()
+                    {
+                        Sender = m.Key,
+                        MessageText = m.Value
+                    })
+                    .ToList()
+            };
+
+            return View(chatModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            _messages.Add(new(chat.CurrentMessage.Sender, chat.CurrentMessage.MessageText));
+            return RedirectToAction(nameof(Show));
+        }
+    }
+}
---
+++ 07.ASP.NetFundamentals/01.ASP.NetIntro/ChatApp/ChatApp/Models/Message/ChatViewModel.cs
@@ -0,0 +1,8 @@
+﻿
+
+public class ChatViewModel
+{
+    public MessageViewModel CurrentMessage { get; set; } = null!;
+    public List<MessageViewModel> Messages { get; set; } = null!;
+}
+
