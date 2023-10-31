17a8eee2aef943bac8b51798492d199a15a09084
Klameta <ivanavalcheva@gmail.com>
2023-10-31 22:46:59 +0200

    01 ,02 done

---
+++ 09.JavaScriptFrontEnd/01.JSSyntaxFundamentals/01.MutliplyTheNumberByTwo/solution.js
@@ -0,0 +1,7 @@
+function solve(inputNumber){
+    console.log(inputNumber*2)
+}
+
+solve(2)
+solve(5)
+solve(20)
---
+++ 09.JavaScriptFrontEnd/01.JSSyntaxFundamentals/02.StudentInformation/solution.js
@@ -0,0 +1,7 @@
+function asign (name, age, grade){
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
+}
+
+asign('John', 15, 5.54678)
+asign('Steve', 16, 2.1426)
+asign('Marry', 12, 6.00 )
