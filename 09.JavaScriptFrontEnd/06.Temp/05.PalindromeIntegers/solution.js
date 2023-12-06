5121dfee61da1bd7726e9bf29f4078e2af821caa
Klameta <ivanavalcheva@gmail.com>
2023-12-07 00:24:01 +0200

    reordering

--- 09.JavaScriptFrontEnd/06.Temp/05.PalindromeIntegers/solution.js
+++
@@ -1,9 +0,0 @@
-function solve(numberArr) {
-    for (const currNum of numberArr) {
-        const currStr = currNum.toString().split("");
-        for (let i = 0; i < currStr.length / 2; i++) {
-            let left = currStr.pop
-
-        }
-    }
-}
