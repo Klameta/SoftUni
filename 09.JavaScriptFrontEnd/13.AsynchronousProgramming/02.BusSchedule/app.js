33901c2964695006cf7180a3d059b97b3f2ebbac
Klameta <ivanavalcheva@gmail.com>
2023-12-15 16:37:53 +0200

    01 fixed 02 done

--- 09.JavaScriptFrontEnd/13.AsynchronousProgramming/02.BusSchedule/app.js
+++ 09.JavaScriptFrontEnd/13.AsynchronousProgramming/02.BusSchedule/app.js
@@ -1,16 +1,42 @@
 function solve() {
-    function depart() {
-        // TODO: 
-    }
+  const baseUrl = "http://localhost:3030/jsonstore/bus/schedule/";
+  let nextBusId = "depot";
+  let currentBusName = "";
 
-    async function arrive() {
-        // TODO:
-    }
+  let departBtn = document.getElementById("depart");
+  let arriveBtn = document.getElementById("arrive");
+  let resultOutput = document.querySelector(".info");
 
-    return {
-        depart,
-        arrive
-    };
+  async function depart() {
+    try {
+      const response = await fetch(baseUrl + nextBusId);
+      const busInfo = await response.json();
+
+      const [name, next] = Object.entries(busInfo);
+
+      nextBusId = next[1];
+      currentBusName = name[1];
+      resultOutput.textContent = `Next stop ${currentBusName}`;
+      departBtn.disabled = "disabled";
+      arriveBtn.disabled = "";
+    } catch (e) {
+      resultOutput.textContent = "Error";
+      departBtn.disabled = "disabled";
+      arriveBtn.disabled = "disabled";
+    }
+  }
+
+  function arrive() {
+    resultOutput.textContent = `Arriving at ${currentBusName}`;
+
+    departBtn.disabled = "";
+    arriveBtn.disabled = "disabled";
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();
